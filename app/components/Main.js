bf9f09eaaae0d8ba0793d3ad1d4eb4f9756a01fe
Rhys Gevaux <rhys.gevaux@gmail.com>
2017-01-03 18:34:34 +1100

    Added UI for navbar

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const styles = {
+	brand: {
+		color: '#fff'
+	},
 	children: {
-		paddingLeft: '0',
-		paddingRight: '0',
-		height: 'calc(100% - 52px)'
+		paddingLeft: 0,
+		paddingRight: 0,
+		height: 'calc(100% - 50px)'
 	},
 	container: {
 		height: '100%',
 	},
 	navbar: {
-		marginBottom: '0'
+		backgroundColor: 'rgba(252, 90, 44, 0.89)',
+		border: 0,
+		borderRadius: 0,
+		marginBottom: 0
 	}
 }
 
@@ -31,11 +37,16 @@ export default function Main(props) {
 							<span className="icon-bar" />
 							<span className="icon-bar" />
 						</button>
-						<a className="navbar-brand" href="#">Main.js header</a>
+						<a className="navbar-brand" href="#" style={styles.brand}>Clever Title</a>
 					</div>
 					<div className="collapse navbar-collapse" id="navbar">
-						<ul className="nav navbar-nav">
-						</ul>
+						<form className="navbar-form navbar-right">
+							<div className="form-group">
+								<label className="sr-only">Enter a City and State</label>
+								<input className="form-control" type="text" placeholder="Melbourne, Australia" />
+							</div>
+							<button className="btn btn-success" type="submit">Get Weather</button>
+						</form>
 					</div>
 				</div>
 			</nav>
