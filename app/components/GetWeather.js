82bbc2c5ee5555f08c868d4fb3811178776f4322
Rhys Gevaux <rhys.gevaux@gmail.com>
2017-01-03 19:11:57 +1100

    Re-factored GetWeather to be a class

--- app/components/GetWeather.js
+++ app/components/GetWeather.js
@@ -1,19 +1,23 @@
-import React, {PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 
-function GetWeather(props) {
-	return (
-		<form className={props.formClass} style={props.formStyle}>
-			<div className="form-group" style={props.groupStyle}>
-				<label className={props.labelClass} style={props.labelStyle}>Enter a City and State</label>
-				<input
-					className="form-control"
-					type="text"
-					placeholder="Melbourne, Australia"
-					style={props.inputStyle} />
-			</div>
-			<button className="btn btn-success" type="submit">Get Weather</button>
-		</form>
-	);
+export default class GetWeather extends Component {
+	render() {
+		return (
+			<form className={this.props.formClass} style={this.props.formStyle}>
+				<div className="form-group" style={this.props.groupStyle}>
+					<label className={this.props.labelClass} style={this.props.labelStyle}>
+						Enter a City and State
+					</label>
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Melbourne, Australia"
+						style={this.props.inputStyle} />
+				</div>
+				<button className="btn btn-success" type="submit">Get Weather</button>
+			</form>
+		);
+	}
 }
 
 GetWeather.propTypes = {
@@ -23,6 +27,13 @@ GetWeather.propTypes = {
 	labelClass: PropTypes. string,
 	labelStyle: PropTypes.object,
 	inputStyle: React.PropTypes.object
-}
+};
 
-export default GetWeather;
+GetWeather.defaultProps = {
+	formClass: '',
+	formStyle: {},
+	groupStyle: {},
+	labelClass: '',
+	labelStyle: {},
+	inputStyle: {}
+}
